f6247a4919326265018441f0e0c3c943849bebae
MatheusL03 <matheusl_3@hotmail.com>
2023-11-28 22:00:52 -0300

    teste commite

---
+++ script.js
@@ -0,0 +1,295 @@
+var cartas = 
+[{nome:'Charmander',
+        imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmPsz2nnKh5YeTlbSUjny3QRQhLeXzSUOZWw&usqp=CAU",
+     atributos:{
+        ataque: 4,
+        defesa: 3, 
+        especial: 4}}, 
+        
+    {nome:'Bulbasaur', 
+          imagem:"https://2.bp.blogspot.com/-stetOQOvAEg/U79C_ctp9RI/AAAAAAAADRw/Ty8BO1q7Yyg/s1600/Bulbasaur-Razor-Leaf.jpg",
+      atributos:{
+          ataque: 3, 
+          defesa: 3, 
+          especial: 4}},
+
+    {nome:'Squirtle', 
+        imagem:"http://pm1.narvii.com/6256/f98def4e1896baf52165256b0e52c06d3383f2c4_00.jpg",
+    atributos:{
+        ataque: 3, 
+        defesa: 4, 
+        especial: 3}},
+
+    {nome:'Cartepie', 
+        imagem:"https://media.pocketmonsters.net/characters/1686.png",
+    atributos:{
+        ataque: 2, 
+        defesa: 3, 
+        especial: 2}},
+
+    {nome:'Weedle', 
+        imagem:"https://theclick.gg/wp-content/uploads/2021/12/Pokemon-Anime-Weedle.jpg",
+    atributos:{
+        ataque: 3, 
+        defesa: 2, 
+        especial: 2}},
+
+    {nome:'Pidgey', 
+        imagem:"https://i.pinimg.com/originals/70/bc/ec/70bceca314b49e1cb7efbbd7dc51a8ea.png",
+    atributos:{
+        ataque: 3, 
+        defesa: 3, 
+        especial: 3}},
+    
+    {nome:'Rattata', 
+        imagem:"https://media.tenor.com/images/7323775f3a0d869feef8347df5d70d63/raw",
+    atributos:{
+        ataque: 4, 
+        defesa: 3, 
+        especial: 2}},
+
+    {nome:'Spearow', 
+        imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEeranf6rcY_e2uYB2HKmqgAAjfG0bmIMVCw&usqp=CAU",
+    atributos:{
+        ataque: 4, 
+        defesa: 2, 
+        especial: 2}},
+
+    {nome:'Ekans', 
+        imagem:"https://i.pinimg.com/originals/bb/f9/0b/bbf90b44fe8d9a6c8669054279539484.png",
+    atributos:{
+        ataque: 4, 
+        defesa: 3, 
+        especial: 3}},
+
+    {nome:'Sandshrew', 
+        imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5G2Hc4B23bY67Izv7-lDBjI1hvukc0edMlwBiPGS0tyy5olMmbI_rqquEzvMXVuXQ3Rs&usqp=CAU",
+    atributos:{
+        ataque: 5, 
+        defesa: 5, 
+        especial: 2}},
+
+    {nome:'Nidoran♀', 
+        imagem:"https://www.giantbomb.com/a/uploads/original/10/103251/1717105-200px_whitney_nidorina.png",
+    atributos:{
+        ataque: 3, 
+        defesa: 4, 
+        especial: 3}},
+
+    {nome:'Nidoran♂', 
+        imagem:"http://pm1.narvii.com/6568/f4e36bbed26423245a7809216202215c27a8758f_00.jpg",
+    atributos:{
+        ataque: 4, 
+        defesa: 3, 
+        especial: 3}},
+
+    {nome:'Clefairy', 
+        imagem:"https://media.pocketmonsters.net/characters/1586.png/t/400.jpg",
+    atributos:{
+        ataque: 3, 
+        defesa: 3, 
+        especial: 4}},
+
+    {nome:'Vulpix', 
+        imagem:"https://pa1.narvii.com/6197/630ad5140dee5085af7513bc41cd7695a6ca9064_hq.gif",
+    atributos:{
+        ataque: 3, 
+        defesa: 3, 
+        especial: 3}},
+
+    {nome:'Jigglypuff', 
+        imagem:"https://images5.fanpop.com/image/photos/26800000/Jigglypuff-anime-26836621-720-480.jpg",
+    atributos:{
+        ataque: 3, 
+        defesa: 2, 
+        especial: 3}},
+
+    {nome:'Zubat', 
+        imagem:"https://i.pinimg.com/736x/a8/75/d2/a875d29402104049d61193622410d754.jpg",
+    atributos:{
+        ataque: 3, 
+        defesa: 3, 
+        especial: 2}},
+
+        {nome:'Oddish', 
+        imagem:"http://pm1.narvii.com/6533/375d81ad2c5387f3af5f6bf928bbdbfa331b366f_00.jpg",
+    atributos:{
+        ataque: 3, 
+        defesa: 4, 
+        especial: 5}},
+
+    {nome:'Paras', 
+        imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoFd3q7TyyEpJIT6tZqwHvsOrV2QYMmjqdp53vFXdOSoFYb83E4YD_yGbZIb3zDuCTKQ&usqp=CAU",
+    atributos:{
+        ataque: 5, 
+        defesa: 4, 
+        especial: 3}},
+
+        
+     {nome:'Venonat', 
+        imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqrqpzQVmNQuLqYwRRDOCVvIoNI712dX1hZA&usqp=CAU",
+    atributos:{
+        ataque: 4, 
+        defesa: 3, 
+        especial: 3}},
+
+        
+     {nome:'Diglett', 
+            imagem:"https://i.ytimg.com/vi/lgZGTHJGMlU/hqdefault.jpg",
+        atributos:{
+            ataque: 4, 
+            defesa: 2, 
+            especial: 3}},
+
+            
+    {nome:'Meowth', 
+        imagem:"https://i1.wp.com/www.jbox.com.br/wp/wp-content/uploads/2020/11/meowth.jpg?resize=774%2C489&quality=99&strip=all&ssl=1",
+    atributos:{
+        ataque: 3, 
+        defesa: 3, 
+        especial: 3}},
+
+        
+     {nome:'Psyduck', 
+            imagem:"https://i.gifer.com/fetch/w300-preview/bf/bf59d24793aaa849808d74f2ca60e0f6.gif",
+        atributos:{
+            ataque: 4, 
+            defesa: 3, 
+            especial: 4}},
+
+        {nome:'Mankey', 
+            imagem:"https://i.redd.it/w3583iamyc961.jpg",
+        atributos:{
+            ataque: 5, 
+            defesa: 3, 
+            especial: 3}},
+
+        {nome:'Growlithe', 
+            imagem:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREzQO71pJgL75ewEKt_cgzO09XbZHlGwV6LPxPqvjUveBwF9W3FulJWc5s95lRzY41Hic&usqp=CAU",
+        atributos:{
+            ataque: 5, 
+            defesa: 3, 
+            especial: 5}},
+
+        {nome:'Poliwag', 
+            imagem:"https://static1.thegamerimages.com/wordpress/wp-content/uploads/2020/10/attacks.jpg?q=50&fit=crop&w=1100&dpr=1.5",
+        atributos:{
+            ataque: 3, 
+            defesa: 3, 
+            especial: 3}},
+
+        {nome:'Abra', 
+            imagem:"https://64.media.tumblr.com/a6d1c6c66499b256597968db093055e3/tumblr_pnj26vYOUt1vw1ceco1_1280.png",
+        atributos:{
+            ataque: 2, 
+            defesa: 1, 
+            especial: 7}},
+
+         {nome:'Machop', 
+            imagem:"https://i.pinimg.com/originals/7a/71/28/7a71288a81589ccb6b5e9b486f0d65fd.jpg",
+        atributos:{
+            ataque: 5, 
+            defesa: 3, 
+            especial: 3}},
+
+    {nome:'Pikachu', 
+        imagem:"https://novidad.es/content/images/wordpress/2013/10/Pikachu.png",
+    atributos:{
+        ataque: 4, 
+        defesa: 3, 
+        especial: 3}
+}]
+
+var cartaMaquina 
+var cartaJogador
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * 29)    
+    cartaMaquina = cartas[numeroCartaMaquina]
+
+
+    var numeroCartaJogador = parseInt(Math.random() * 29)  
+    while(numeroCartaMaquina == numeroCartaJogador){
+        numeroCartaJogador = parseInt(Math.random() * 29)
+    } 
+
+    cartaJogador = cartas[numeroCartaJogador]
+    console.log(cartaJogador)
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+
+    exibirCartaJogador()
+}
+
+
+function obtemAtributoSelecionado(){
+ var radioAtributos = document.getElementsByName('atributo')
+
+for (var i = 0; i < radioAtributos.length; i++){
+if (radioAtributos[i].checked == true){
+    return radioAtributos[i].value
+  }
+ }
+}
+
+
+function jogar(){
+    var atributoSelcionado = obtemAtributoSelecionado()
+    var res = document.getElementById('resultado')
+    var valorCartaJogador = cartaJogador.atributos[atributoSelcionado]
+    var valorCartaMaquina = cartaMaquina.atributos[atributoSelcionado]
+
+    if (valorCartaJogador > valorCartaMaquina){
+        htmlResultados = "<p class='resultado-final'>Você Venceu!</p>"
+       
+    } else if (valorCartaJogador < valorCartaMaquina){
+        htmlResultados = "<p class='resultado-final'>Você Perdeu!</p>"
+     
+    }else{
+        htmlResultados = "<p class='resultado-final'>Empatou!</p>"
+      
+    }
+
+    res.innerHTML = htmlResultados
+
+    document.getElementById('btnJogar').disabled = true
+
+    exibirCartaMaquina()
+}
+
+function exibirCartaJogador(){
+   var divCartaJogador = document.getElementById('carta-jogador')
+   divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+
+   var moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+
+   var tagHTML = "<div id='opcoes' class='carta-status'>"
+
+   var opcoesTexto = ''
+ for(var atributo in cartaJogador.atributos){
+     opcoesTexto += "<input type='radio' name='atributo'value='" +atributo + "'>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+
+}
+
+function exibirCartaMaquina(){
+    var divCartaMaquina = document.getElementById('carta-maquina')
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+ 
+    var moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+ 
+    var tagHTML = "<div id='opcoes' class='carta-status'>"
+ 
+    var opcoesTexto = ''
+  for(var atributo in cartaMaquina.atributos){
+      opcoesTexto += "<p type='text' name='atributo'value='" +atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "</p>";
+   }
+   var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+ 
+   divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+ 
+}
